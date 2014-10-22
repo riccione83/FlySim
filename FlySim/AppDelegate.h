1eb85ebbb0cc6dcc140259d804f6036bffb3814b
Riccardo Rizzo <r.riki@tiscali.it>
2014-10-22 10:21:55 +0200

    Initial Commit

---
+++ FlySim/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlySim
+//
+//  Created by Riccardo Rizzo on 22/10/14.
+//  Copyright (c) 2014 Riccardo Rizzo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlySim/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  FlySim
+//
+
+//  Copyright (c) 2014 Riccardo Rizzo. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
